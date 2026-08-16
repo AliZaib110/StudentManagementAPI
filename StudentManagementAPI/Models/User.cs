a5185efb412d65118436bbb4ef57009900e9d05a
alizaib110 <alizaibbuttar9@gmail.com>
2026-08-17 00:21:46 +0500

    create user model

---
+++ StudentManagementAPI/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace StudentManagementAPI.Models
+{
+    public class User
+    {
+        public int Id { set; get; }
+        public string Name { set; get; } = string.Empty;
+        public string Email { set; get; } = string.Empty;
+        public string PasswordHash { set; get; } = string.Empty;
+        public string Role { set; get; } = string.Empty;
+
+
+    }
+}
