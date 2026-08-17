b0da97c7d5a0b850714789bfb3149774cda8b88b
alizaib110 <alizaibbuttar9@gmail.com>
2026-08-18 00:10:57 +0500

    add user in dbcontext

--- StudentManagementAPI/Data/ApplicationDbContext.cs
+++ StudentManagementAPI/Data/ApplicationDbContext.cs
@@ -2,14 +2,16 @@
 using StudentManagementAPI.Models;
 
 namespace StudentManagementAPI.Data;
-    public class ApplicationDbContext : DbContext
+
+public class ApplicationDbContext : DbContext
 {
-    
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        {
 
-        }
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
 
-        public DbSet<Student> Students { get; set; }
-    
+    }
+
+    public DbSet<Student> Students { get; set; }
+    public DbSet<User> Users { get; set; }
+
 }
