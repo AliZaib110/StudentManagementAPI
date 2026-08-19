49cf2c90ccc85d84be88a5c20faa441c7750c029
alizaib110 <alizaibbuttar9@gmail.com>
2026-08-20 00:29:30 +0500

    create interface and service

---
+++ StudentManagementAPI/Interfaces/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿using StudentManagementAPI.DTOs;
+
+namespace StudentManagementAPI.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<bool> Register(RegisterDto dto);
+    }
+}
---
+++ StudentManagementAPI/Services/AuthService.cs
@@ -0,0 +1,13 @@
+﻿using StudentManagementAPI.DTOs;
+using StudentManagementAPI.Interfaces;
+
+namespace StudentManagementAPI.Services
+{
+    public class AuthService : IAuthService
+    {
+        public Task<bool> Register(RegisterDto dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
